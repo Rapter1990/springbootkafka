392a5d2a83b50929082c0f9aaa21b26613cf0306
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-09 12:28:46 +0300

    Initial commit Task 1 : Add Apache Kafka

---
+++ src/main/java/com/springboot/kafka/springbootkafka/SpringbootkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootkafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootkafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/kafka/springbootkafka/SpringbootkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootkafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
