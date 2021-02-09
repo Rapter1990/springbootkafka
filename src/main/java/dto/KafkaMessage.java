f44f2dc0f9fe6261e54b2100ace65fa7b2afc65b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-09 12:48:31 +0300

    Task 2 : Create KafkaMessage

---
+++ src/main/java/dto/KafkaMessage.java
@@ -0,0 +1,29 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class KafkaMessage {
+
+    private String message;
+
+    private String id = UUID.randomUUID().toString();
+
+    private LocalDateTime messageDate = LocalDateTime.now();
+
+    @Override
+    public String toString() {
+        return "KafkaMessage{" +
+                "message='" + message + '\'' +
+                ", id='" + id + '\'' +
+                ", messageDate=" + messageDate +
+                '}';
+    }
+}
