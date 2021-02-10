aaf3d3181027c0c812ae0ebf69dc7839efe47de9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-10 13:53:01 +0300

    Task 7 : Create KafkaController and KafkaService

---
+++ src/main/java/com/springboot/kafka/springbootkafka/controller/KafkaController.java
@@ -0,0 +1,52 @@
+package com.springboot.kafka.springbootkafka.controller;
+
+import com.springboot.kafka.springbootkafka.dto.KafkaMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/kafkamessage")
+public class KafkaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaController.class);
+
+    @Value("${k.kafka.topic}")
+    private String kafkaTopic;
+
+    @Autowired
+    private KafkaTemplate<String, KafkaMessage> kafkaTemplate;
+
+    @PostMapping
+    public void sendMessage(@RequestBody KafkaMessage kafkaMessage) {
+
+        UUID messageUUID = UUID.randomUUID();
+
+        ListenableFuture<SendResult<String, KafkaMessage>> future = kafkaTemplate.send(kafkaTopic, messageUUID.toString(), kafkaMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, KafkaMessage>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, KafkaMessage> result) {
+                logger.info("Sent message=[{}] with offset=[{}}]", kafkaMessage, result.getRecordMetadata().offset());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                logger.error("Unable to send message=[{}] due to : {}", kafkaMessage, ex.getMessage());
+            }
+        });
+
+    }
+}
---
+++ src/main/java/com/springboot/kafka/springbootkafka/service/KafkaService.java
@@ -0,0 +1,30 @@
+package com.springboot.kafka.springbootkafka.service;
+
+import com.springboot.kafka.springbootkafka.dto.KafkaMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class KafkaService {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+    @KafkaListener(
+            topicPartitions = @TopicPartition(
+                    topic = "${fd.kafka.topic}",
+                    partitionOffsets = { @PartitionOffset(
+                            partition = "0",
+                            initialOffset = "0") }),
+            groupId = "${k.kafka.group.id}"
+    )
+    public void listen(@Payload KafkaMessage kafkaMessage) {
+        logger.info("MessageInfo : {}", kafkaMessage);
+    }
+}
